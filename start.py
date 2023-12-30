f91f82aea290c55d6c9a9760e44d3a45dd2c1fc8
Kumarakuru <Kumarakuru@gmail.com>
2023-12-30 08:38:40 +0000

    Introduced text box

--- start.py
+++ start.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import openai
 
+title = st.text_input('Movie title', 'Life of Brian')
+st.write('The current movie title is', title)
+
 st.write("OK Imported !!!")
 
 st.title("Kumara Personal Info !!!")
