d301a3ff2aab48814ad7a68c475aae95eff5dfab
Kumarakuru <Kumarakuru@gmail.com>
2024-01-01 15:10:56 +0800

    update with langchain

--- start.py
+++ start.py
@@ -1,8 +1,18 @@
 import streamlit as st
 import openai
 import streamlit.components.v1 as components 
+from langchain.document_loaders import TextLoader
+from langchain.indexes  import VectorstoreIndexCreator
+import openai, os
 
-initial_text="This is start only !!!"
+#setup Open AI
+ai_key="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"
+openai.api_key = ai_key
+os.environ["OPENAI_API_KEY"] = ai_key;
+
+#Initialize langchain
+loader = TextLoader(r'cv.json')
+index = VectorstoreIndexCreator().from_loaders([loader])
 
 # Open a file: file
 file = open('cv.html',mode='r')
@@ -11,15 +21,13 @@ cv_text = file.read()
 # close the file
 file.close() 
 
-query = st.text_input('You wanted to know specifics, enter your question', '')
+query = st.text_input("What's on Your Mind? Ask Me About Anything Specific", '')
 html_string=F"<div font-size=3px>Powered by chatGPT</div><br>Response for your query is:<b>{query}</b>"
 st.markdown(html_string, unsafe_allow_html=True);
 
 
-if len(query)>0:
-    cv_text="Query Done !!!"
+components.html(index.query(query),height=500,scrolling=True)
 
-#st.markdown(cv_text,unsafe_allow_html=True);
 
 components.html(cv_text,height=2000,scrolling=True )
     
